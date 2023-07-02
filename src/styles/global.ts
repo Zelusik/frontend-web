aaf37f489ed26369e44d7b9321da19c86870b7ec
gabi <70315572+Limgabi@users.noreply.github.com>
2023-07-03 01:41:36 +0900

    style : global css 수정

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,14 +1,26 @@
 import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
 const GlobalStyles = createGlobalStyle`
-  html, body, div, p {
-    margin: 0;
-    padding: 0;
+  ${reset},
+  *,
+  *::before,
+  *::after {
     box-sizing: border-box;
   }
-
-  :root {
-    --color__primary: #E75151;
+  html, body { max-width: 100%; }
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+  body * {
+    font-family: Pretendard; 
+  }
+  #__next {
+    height: 100%;
+  }
+  ol,ul,li {
+    list-style: none;
   }
 `;
 
