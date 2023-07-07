71263eb8bb38aa513f06f56b7388beb96cd15934
gabi <70315572+Limgabi@users.noreply.github.com>
2023-07-07 21:44:12 +0900

    fix : Icon props 추가

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -8,12 +8,13 @@ interface IconProps {
   width?: number;
   height?: number;
   rotate?: number;
+  color?: string;
 }
 
-const Icon = ({ icon, width, height, rotate }: IconProps) => {
+const Icon = ({ icon, width, height, rotate, color }: IconProps) => {
   const IconComponent = icons[icon as keyof typeof icons];
   return (
-    <IconWrapper width={width} height={height} rotate={rotate}>
+    <IconWrapper width={width} height={height} rotate={rotate} color={color}>
       <IconComponent />
     </IconWrapper>
   );
@@ -23,6 +24,7 @@ const IconWrapper = styled.div<{
   width?: number;
   height?: number;
   rotate?: number;
+  color?: string;
 }>`
   display: flex;
   justify-content: center;
@@ -38,6 +40,10 @@ const IconWrapper = styled.div<{
   >svg {
     width: ${({ width }) => width}px;
     height: ${({ height }) => height}px;
+
+    path {
+      fill: ${({ color }) => color};
+    }
   }
 `;
 
