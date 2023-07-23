8e52752a6648a48459a3f7ecf5a40ab452950546
cyeju <yeju1019@gmail.com>
2023-07-23 22:26:25 +0900

    feat: search-place 추가

--- src/constants/Route.ts
+++ src/constants/Route.ts
@@ -1,8 +1,11 @@
 export const Route = {
   HOME: () => "/",
+  HOME_DETAIL: () => "/home-detail",
+
   MAP: () => "/map",
+  SearchPlace: () => "/search-place",
+
   REVIEW: () => "/review",
   MARK: () => "/mark",
   MYPAGE: () => "/mypage",
-  HOME_DETAIL: () => "/home-detail",
 };
---
+++ src/pages-edit/SearchPlace/SearchPlace.tsx
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router";
+
+export default function SearchPlace() {
+  const router = useRouter();
+
+  return (
+    <>
+      <div>SearchPlace</div>
+    </>
+  );
+}
