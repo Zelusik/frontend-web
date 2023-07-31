d3ee25a603d366d12b00a9a26bc0b570f906fee0
gabi <70315572+Limgabi@users.noreply.github.com>
2023-07-31 15:13:58 +0900

    feat : 리뷰 메뉴 태그를 위한 상태관리

--- src/reducer/slices/image/imageSlice.ts
+++ src/reducer/slices/image/imageSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { ImageType } from "types/image";
+import { ImageType, MenuTagType } from "types/image";
 
 // 이미지를 보여줄 path, 위도 경도 등을 저장할 slice
 const initialState: ImageType[] = [];
@@ -12,9 +12,74 @@ export const imageSlice = createSlice({
     changeImageInfo: (state, { payload }: { payload: ImageType }) => {
       state.push(payload);
     },
+    appendMenuTag: (
+      state,
+      {
+        payload,
+      }: {
+        payload: ImageType & {
+          index: number;
+          menuTag: MenuTagType;
+        };
+      }
+    ) => {
+      const { index, menuTag } = payload;
+      if (!state[index].menuTag) {
+        state[index].menuTag = [];
+      }
+      state[index].menuTag.push(menuTag);
+    },
+    deleteMenuTag: (
+      state,
+      {
+        payload,
+      }: {
+        payload: ImageType & {
+          index: number;
+          menuTag: MenuTagType;
+        };
+      }
+    ) => {
+      const { index, menuTag } = payload;
+      state[index].menuTag = state[index].menuTag.filter(
+        (tag) =>
+          !(tag.x === menuTag.x && tag.y === menuTag.y && tag.menu === menuTag.menu)
+      );
+    },
+    modifyMenuTag: (
+      state,
+      {
+        payload,
+      }: {
+        payload: ImageType & {
+          index: number;
+          prevMenuTag: MenuTagType;
+          currMenuTag: MenuTagType;
+        };
+      }
+    ) => {
+      const { index, prevMenuTag, currMenuTag } = payload;
+      state[index].menuTag = state[index].menuTag.map((tag) => {
+        if (
+          tag.x === prevMenuTag.x &&
+          tag.y === prevMenuTag.y &&
+          tag.menu === prevMenuTag.menu
+        ) {
+          return currMenuTag;
+        } else {
+          return tag;
+        }
+      });
+    },
   },
 });
 
-export const { initializeImageInfo, changeImageInfo } = imageSlice.actions;
+export const {
+  initializeImageInfo,
+  changeImageInfo,
+  appendMenuTag,
+  deleteMenuTag,
+  modifyMenuTag,
+} = imageSlice.actions;
 
 export default imageSlice.reducer;
