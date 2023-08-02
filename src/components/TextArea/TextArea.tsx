d68c6b62c4ab945dc13668067e4ecf04bde34704
gabi <70315572+Limgabi@users.noreply.github.com>
2023-08-02 12:12:35 +0900

    style : 디자인시스템 수정

--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -4,23 +4,34 @@ import { typography } from "constants/typography";
 import Spacing from "components/Spacing";
 import { colors } from "constants/colors";
 
-export default function TextArea({ size, text, placeholder, onChange }: any) {
+export default function TextArea({
+  size,
+  text,
+  placeholder,
+  onChange,
+  borderColor,
+}: any) {
   return (
     <>
-      <TextAreaWrapper placeholder={placeholder} size={size} onChange={onChange} />
+      <TextAreaWrapper
+        placeholder={placeholder}
+        size={size}
+        onChange={onChange}
+        borderColor={borderColor}
+      />
       <Spacing size={12} />
       <BoxText>{text}</BoxText>
     </>
   );
 }
 
-const TextAreaWrapper = styled.textarea<{ size: number }>`
+const TextAreaWrapper = styled.textarea<{ size: number; borderColor?: string }>`
   width: 100%;
   height: ${({ size }) => size}px;
   padding: 13px 14px;
 
   border-radius: 12px;
-  border: 1px solid ${colors.N40};
+  border: 1px solid ${({ borderColor }) => (borderColor ? borderColor : colors.N40)};
   background-color: ${colors.N10};
   resize: none;
 `;
