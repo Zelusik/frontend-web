f86a417d6cbdd5da338663bcfdb62e61c3649835
gabi <70315572+Limgabi@users.noreply.github.com>
2023-08-21 00:21:47 +0900

    feat : 무한스크롤 적용

--- src/hooks/queries/mark/useGetMarkPlaces.ts
+++ src/hooks/queries/mark/useGetMarkPlaces.ts
@@ -1,51 +1,34 @@
 import React from "react";
 import { getMarkPlaces } from "api/places";
-import { useRouter } from "next/router";
 import { useInfiniteQuery } from "react-query";
-import { useQuery } from "react-query";
 
 const useGetMarkPlaces = ({ currentIndex, type, keyword }: any) => {
-  const { data, isLoading, error, refetch } = useQuery(
-    [currentIndex],
-    async () => {
-      if (keyword !== "") {
-        const params: any = {
-          type: type,
-          keyword: keyword,
-          page: 0,
-          size: 10,
-        };
-
-        const result = getMarkPlaces(params);
-        return result;
-      }
+  const fetchMarkPlaces = async ({ pageParam = 0 }) => {
+    if (keyword !== "") {
+      const params: any = {
+        type: type,
+        keyword: keyword,
+        page: pageParam,
+        size: 20,
+      };
+      return getMarkPlaces(params);
     }
-  );
+  };
 
-  return { data, isLoading, error, refetch };
+  const {
+    data: responseData,
+    isLoading,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    refetch,
+  } = useInfiniteQuery([currentIndex, type, keyword], fetchMarkPlaces, {
+    getNextPageParam: (lastPage) => {
+      return lastPage.isLast ? undefined : lastPage.number + 1;
+    },
+  });
+  const data = responseData?.pages;
+  return { data, isLoading, error, fetchNextPage, hasNextPage, refetch };
 };
-// const {
-//   data: responseData,
-//   fetchNextPage,
-//   hasNextPage,
-// } = useInfiniteQuery(
-//   ["markPlaces", currentIndex],
-//   async ({ pageParam = 0 }) => {
-//     return await getMarkPlaces({
-//       type: type,
-//       keyword: keyword,
-//       page: pageParam,
-//       size: 20,
-//     });
-//   },
-
-//   {
-//     getNextPageParam: (lastPage) => {
-//       return lastPage.isLast ? undefined : lastPage.number + 1;
-//     },
-//   }
-// );
-// const data = responseData?.pages;
-// return { data, fetchNextPage, hasNextPage };
 
 export default useGetMarkPlaces;
