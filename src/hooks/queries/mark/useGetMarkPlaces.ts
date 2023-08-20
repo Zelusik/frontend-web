ea71be70224e096bb683447131596992004890bc
cyeju <yeju1019@gmail.com>
2023-08-20 23:49:06 +0900

    feat: mark 페이지

--- src/hooks/queries/mark/useGetMarkPlaces.ts
+++ src/hooks/queries/mark/useGetMarkPlaces.ts
@@ -2,37 +2,50 @@ import React from "react";
 import { getMarkPlaces } from "api/places";
 import { useRouter } from "next/router";
 import { useInfiniteQuery } from "react-query";
+import { useQuery } from "react-query";
 
-const useGetMarkPlaces = () => {
-  const { query } = useRouter();
-  const type = Array.isArray(query.type) ? query.type[0] : query.type || "";
-  const keyword = Array.isArray(query.keyword)
-    ? query.keyword[0]
-    : query.keyword || "";
+const useGetMarkPlaces = ({ currentIndex, type, keyword }: any) => {
+  const { data, isLoading, error, refetch } = useQuery(
+    [currentIndex],
+    async () => {
+      if (keyword !== "") {
+        const params: any = {
+          type: type,
+          keyword: keyword,
+          page: 0,
+          size: 10,
+        };
 
-  const {
-    data: responseData,
-    fetchNextPage,
-    hasNextPage,
-  } = useInfiniteQuery(
-    ["markPlaces", query],
-    async ({ pageParam = 0 }) => {
-      return await getMarkPlaces({
-        type: type,
-        keyword: keyword,
-        page: pageParam,
-        size: 20,
-      });
-    },
-
-    {
-      getNextPageParam: (lastPage) => {
-        return lastPage.isLast ? undefined : lastPage.number + 1;
-      },
+        const result = getMarkPlaces(params);
+        return result;
+      }
     }
   );
-  const data = responseData?.pages;
-  return { data, fetchNextPage, hasNextPage };
+
+  return { data, isLoading, error, refetch };
 };
+// const {
+//   data: responseData,
+//   fetchNextPage,
+//   hasNextPage,
+// } = useInfiniteQuery(
+//   ["markPlaces", currentIndex],
+//   async ({ pageParam = 0 }) => {
+//     return await getMarkPlaces({
+//       type: type,
+//       keyword: keyword,
+//       page: pageParam,
+//       size: 20,
+//     });
+//   },
+
+//   {
+//     getNextPageParam: (lastPage) => {
+//       return lastPage.isLast ? undefined : lastPage.number + 1;
+//     },
+//   }
+// );
+// const data = responseData?.pages;
+// return { data, fetchNextPage, hasNextPage };
 
 export default useGetMarkPlaces;
--- src/pages-edit/Mark/Mark.tsx
+++ src/pages-edit/Mark/Mark.tsx
@@ -1,58 +1,236 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useRouter } from "next/router";
-import BottomNavigation from "components/BottomNavigation";
-import styled from "@emotion/styled";
-import SortingHeader from "./components/SortingHeader";
-import FoodComponents from "./components/FoodComponents";
-import useGetMarkPlaces from "hooks/queries/mark/useGetMarkPlaces";
-import SearchTitle from "components/Title/SearchTitle";
-import TopNavigation from "components/TopNavigation/TopNavigation";
+// import { useRouter } from "next/router";
+// import BottomNavigation from "components/BottomNavigation";
+// import styled from "@emotion/styled";
+// import SortingHeader from "./components/SortingHeader";
+// import StoreCard from "./components/StoreCard";
+// import useGetMarkPlaces from "hooks/queries/mark/useGetMarkPlaces";
+// import SearchTitle from "components/Title/SearchTitle";
+// import TopNavigation from "components/TopNavigation/TopNavigation";
+// import { useEffect, useRef, useState } from "react";
+// import useGetMarkKeywords from "hooks/queries/mark/useGetMarkKeywords";
+// import Spacing from "components/Spacing/Spacing";
+// import useIntersectionObserver from "hooks/useIntersectionObserver";
+// import NewButton from "pages-edit/Mypage/components/NewButton";
+// import Text from "components/Text/Text";
+// import { Route } from "constants/Route";
+// import useDisplaySize from "hooks/useDisplaySize";
+// import { globalValue } from "constants/globalValue";
+
+// export default function Mark() {
+//   const router = useRouter();
+//   const { height } = useDisplaySize();
+//   const infiniteScrollRef = useRef<any>(null);
+//   const scrollRef = useRef<any>(null);
+//   const [topFixed, setTopFixed] = useState<boolean>(false);
+//   const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+//   // const { data: keywordData } = useGetMarkKeywords();
+//   // const keywords = keywordData && [
+//   //   {
+//   //     keyword: "전체",
+//   //     type: "",
+//   //   },
+//   //   ...keywordData?.keywords,
+//   // ];
+
+//   const keywords = [
+//     {
+//       keyword: "전체",
+//       type: "",
+//     },
+//     {
+//       keyword: "전체",
+//       type: "",
+//     },
+//     {
+//       keyword: "전체",
+//       type: "",
+//     },
+//   ];
+//   const keywordList = keywords?.map(
+//     (keywordInfo: { keyword: string; type: string }) => keywordInfo.keyword
+//   );
+
+//   const {
+//     data: placeData,
+//     // fetchNextPage,
+//     // hasNextPage,
+//   } = useGetMarkPlaces({
+//     currentIndex,
+//     type: keywords?.[currentIndex]?.type,
+//     keyword: keywords?.[currentIndex]?.keyword,
+//   });
+
+//   // useIntersectionObserver(infiniteScrollRef, fetchNextPage, !!hasNextPage, {});
+
+//   function onScroll() {
+//     const scrollTop = 20;
+
+//     // if (currentIndex === 0 && scrollRef.current?.scrollTop >= scrollTop) {
+//     //   scrollRef.current!.scrollTop = scrollTop;
+//     //   return;
+//     // }
+
+//     if (scrollRef.current?.scrollTop > scrollTop) {
+//       setTopFixed(true);
+//     } else {
+//       setTopFixed(false);
+//     }
+//   }
+
+//   useEffect(() => {
+//     scrollRef.current?.addEventListener("scroll", onScroll);
+//     return () => {
+//       scrollRef.current?.removeEventListener("scroll", onScroll);
+//     };
+//   }, [currentIndex]);
+
+//   return (
+//     <>
+//       {placeData && (
+//         <>
+//           <SearchTitle type="mark" />
+//           <Wrapper
+//             ref={scrollRef}
+//             height={height - 50 - globalValue.BOTTOM_NAVIGATION_HEIGHT}
+//           >
+//             <Spacing size={20} />
+//             <TopNavigation
+//               type="mark"
+//               scrollRef={scrollRef}
+//               scrollTop={20}
+//               currentIndex={currentIndex}
+//               setCurrentIndex={setCurrentIndex}
+//               topFixed={topFixed}
+//               titleList={["전체", "전체", "전체"]}
+//               count={placeData?.totalElements}
+//             >
+//               {keywordList &&
+//                 keywordList.map((keyword: string, idx: number) => (
+//                   <TopNavigationInner key={keyword}>
+//                     <Spacing size={19} />
+//                     {currentIndex === idx && placeData?.contents.length > 0 ? (
+//                       <div>Hi</div>
+//                     ) : (
+//                       // <div className="place-box">
+//                       //   {placeData.map((place) =>
+//                       //     place?.map((placeInfo: any) => (
+//                       //       <StoreCard
+//                       //         key={placeInfo.id}
+//                       //         placeInfo={placeInfo}
+//                       //       />
+//                       //     ))
+//                       //   )}
+//                       //   <div
+//                       //     ref={infiniteScrollRef}
+//                       //     style={{ height: "30px" }}
+//                       //   />
+//                       // </div>
+//                       <NoContent>
+//                         <Text typo="Paragraph5" color="N80">
+//                           아직 저장된 음식점이 없습니다
+//                         </Text>
+//                         <NewButton
+//                           buttonText="음식점 리뷰 둘러보기"
+//                           onClick={() => router.push(Route.HOME())}
+//                         />
+//                       </NoContent>
+//                     )}
+//                   </TopNavigationInner>
+//                 ))}
+//             </TopNavigation>
+//           </Wrapper>
+//           <BottomNavigation />
+//         </>
+//       )}
+//     </>
+//   );
+// }
+
+// const Wrapper = styled.div<{ height: number }>`
+//   height: ${({ height }) => height}px;
+//   overflow-y: scroll;
+//   background-color: #fbfbfb;
+//   overflow-y: scroll;
+//   .place-box {
+//     display: flex;
+//     flex-direction: column;
+//     gap: 20px;
+//     padding: 0 15px;
+//     height: 100%;
+//   }
+// `;
+
+// const NoContent = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   gap: 20px;
+
+//   height: calc(100vh - 227px);
+//   text-align: center;
+// `;
+
+// const TopNavigationInner = styled.div`
+//   height: 100%;
+// `;
+
 import { useEffect, useRef, useState } from "react";
-import useGetMarkKeywords from "hooks/queries/mark/useGetMarkKeywords";
-import Spacing from "components/Spacing/Spacing";
-import useIntersectionObserver from "hooks/useIntersectionObserver";
-import NewButton from "pages-edit/Mypage/components/NewButton";
-import Text from "components/Text/Text";
-import { Route } from "constants/Route";
+import { useRouter } from "next/router";
+import styled from "@emotion/styled";
 import useDisplaySize from "hooks/useDisplaySize";
+
+import { colors } from "constants/colors";
+import TopNavigation from "components/TopNavigation";
+import SearchTitle from "components/Title/SearchTitle";
+import useGetMarkKeywords from "hooks/queries/mark/useGetMarkKeywords";
+import Spacing from "components/Spacing";
+import useGetMarkPlaces from "hooks/queries/mark/useGetMarkPlaces";
+import Text from "components/Text";
+import NewButton from "pages-edit/Mypage/components/NewButton";
+import StoreCard from "./components/StoreCard";
+import BottomNavigation from "components/BottomNavigation";
 import { globalValue } from "constants/globalValue";
 
 export default function Mark() {
   const router = useRouter();
+  const scrollRef = useRef<any>(null);
   const { height } = useDisplaySize();
-  const { data: placeData, fetchNextPage, hasNextPage } = useGetMarkPlaces();
-  const { data: keywordData } = useGetMarkKeywords();
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const [topFixed, setTopFixed] = useState<boolean>(false);
 
+  const { data: keywordData } = useGetMarkKeywords();
   const keywords = keywordData && [
     {
       keyword: "전체",
       type: "",
     },
-    ...keywordData.keywords,
+    ...keywordData?.keywords,
   ];
+  const keywordList = keywords?.map(
+    (keywordInfo: { keyword: string; type: string }) => keywordInfo.keyword
+  );
 
-  const keywordList =
-    keywords &&
-    keywords.map(
-      (keywordInfo: { keyword: string; type: string }) => keywordInfo.keyword
-    );
-
-  const infiniteScrollRef = useRef<any>(null);
-  const scrollRef = useRef<any>(null);
-  const [topFixed, setTopFixed] = useState<boolean>(false);
-  const [currentIndex, setCurrentIndex] = useState<number>(0);
-
-  useIntersectionObserver(infiniteScrollRef, fetchNextPage, !!hasNextPage, {});
+  const {
+    data: placeData,
+    // fetchNextPage,
+    // hasNextPage,
+  } = useGetMarkPlaces({
+    currentIndex,
+    type: keywords?.[currentIndex]?.type,
+    keyword: keywords?.[currentIndex]?.keyword,
+  });
 
   function onScroll() {
-    const scrollTop = 50 + 20 + 35;
+    const scrollTop = 20;
 
-    if (currentIndex === 0 && scrollRef.current?.scrollTop >= scrollTop) {
-      scrollRef.current!.scrollTop = scrollTop;
-      return;
-    }
+    // if (currentIndex === 0 && scrollRef.current?.scrollTop >= scrollTop) {
+    //   scrollRef.current!.scrollTop = scrollTop;
+    //   return;
+    // }
 
-    if (scrollRef.current?.scrollTop > scrollTop - 30) {
+    if (scrollRef.current?.scrollTop > scrollTop) {
       setTopFixed(true);
     } else {
       setTopFixed(false);
@@ -60,18 +238,6 @@ export default function Mark() {
   }
 
   useEffect(() => {
-    if (keywords) {
-      const keywordInfo = keywords[currentIndex];
-      router.push({
-        pathname: router.pathname,
-        query: {
-          ...router.query,
-          type: keywordInfo.type,
-          keyword: keywordInfo.keyword,
-        },
-      });
-    }
-
     scrollRef.current?.addEventListener("scroll", onScroll);
     return () => {
       scrollRef.current?.removeEventListener("scroll", onScroll);
@@ -80,71 +246,66 @@ export default function Mark() {
 
   return (
     <>
-      {placeData && (
-        <>
-          <SearchTitle type="mark" />
-          <MarkWrapper height={height - 50 - globalValue.BOTTOM_NAVIGATION_HEIGHT}>
-            <Spacing size={20} />
-            <TopNavigation
-              type="mark"
-              scrollRef={scrollRef}
-              scrollTop={70}
-              currentIndex={currentIndex}
-              setCurrentIndex={setCurrentIndex}
-              topFixed={topFixed}
-              titleList={keywordList}
-            >
-              {keywords &&
-                keywordList &&
-                keywordList.map((keyword: string) => (
-                  <TopNavigationInner key={keyword}>
-                    <SortingHeader count={placeData?.[0].totalElements} />
-                    {placeData && placeData?.length > 0 ? (
-                      <div className="place-box">
-                        {placeData.map((place) =>
-                          place.contents.map((placeInfo: any) => (
-                            <FoodComponents
-                              key={placeInfo.id}
-                              placeInfo={placeInfo}
-                            />
-                          ))
+      <SearchTitle type="mark" />
+
+      <Wrapper
+        ref={scrollRef}
+        height={height - 50 - globalValue.BOTTOM_NAVIGATION_HEIGHT}
+      >
+        <Spacing size={20} />
+        <TopNavigation
+          type="mark"
+          scrollRef={scrollRef}
+          scrollTop={20}
+          topFixed={topFixed}
+          currentIndex={currentIndex}
+          setCurrentIndex={setCurrentIndex}
+          titleList={keywordList}
+          count={placeData?.totalElements ? placeData?.totalElements : 0}
+        >
+          {keywords?.map((_: any, idx: number) => {
+            return (
+              <TopNavigationInner key={idx} height={"auto"}>
+                {placeData?.totalElements !== 0 ? (
+                  currentIndex === idx ? (
+                    <>
+                      <Spacing size={19} />
+                      <StoreWrapper>
+                        {placeData?.contents?.map(
+                          (place: any, placeIdx: number) => {
+                            return (
+                              <StoreCard key={placeIdx} placeInfo={place} />
+                            );
+                          }
                         )}
-                        <div ref={infiniteScrollRef} style={{ height: "30px" }} />
-                      </div>
-                    ) : (
-                      <NoContent>
-                        <Text typo="Paragraph5" color="N80">
-                          아직 저장된 음식점이 없습니다
-                        </Text>
-                        <NewButton
-                          buttonText="음식점 리뷰 둘러보기"
-                          onClick={() => router.push(Route.HOME())}
-                        />
-                      </NoContent>
-                    )}
-                  </TopNavigationInner>
-                ))}
-            </TopNavigation>
-          </MarkWrapper>
-          <BottomNavigation />
-        </>
-      )}
+                      </StoreWrapper>
+                      <Spacing size={20} />
+                    </>
+                  ) : undefined
+                ) : (
+                  <NoContent>
+                    <Text typo="Paragraph5" color="N80">
+                      아직 저장된 음식점이 없습니다
+                    </Text>
+                    <NewButton
+                      buttonText="음식점 리뷰 둘러보기"
+                      onClick={() => router.push(Route.HOME())}
+                    />
+                  </NoContent>
+                )}
+              </TopNavigationInner>
+            );
+          })}
+        </TopNavigation>
+      </Wrapper>
+      <BottomNavigation />
     </>
   );
 }
-
-const MarkWrapper = styled.div<{ height: number }>`
-  height: ${({ height }) => height + "px"};
-  overflow-y: auto;
-  background-color: #fbfbfb;
+const Wrapper = styled.div<{ height: number }>`
+  height: ${({ height }) => height}px;
   overflow-y: scroll;
-  .place-box {
-    display: flex;
-    flex-direction: column;
-    gap: 24px;
-    padding: 0 15px;
-    height: 100%;
-  }
+  background-color: #fbfbfb;
 `;
 
 const NoContent = styled.div`
@@ -153,10 +314,18 @@ const NoContent = styled.div`
   justify-content: center;
   gap: 20px;
 
-  height: calc(100vh - 247px);
+  height: calc(100vh - 227px);
   text-align: center;
 `;
 
-const TopNavigationInner = styled.div`
-  height: 100%;
+const TopNavigationInner = styled.div<{ height: any }>`
+  height: ${({ height }) => height};
+  padding: 0 20px;
+  background-color: #fbfbfb;
+`;
+
+const StoreWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 `;
