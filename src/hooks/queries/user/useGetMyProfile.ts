f69ae7e8c080624da9f14e8fe12fd20ae568687a
gabi <70315572+Limgabi@users.noreply.github.com>
2023-08-29 23:46:34 +0900

    fix : 마이페이지 분기처리

--- src/hooks/queries/user/useGetMyProfile.ts
+++ src/hooks/queries/user/useGetMyProfile.ts
@@ -1,14 +1,18 @@
 import React from "react";
 import { useQuery } from "react-query";
 import { getMyProfile } from "api/members";
+import { useRouter } from "next/router";
 
 const useGetMyProfile = () => {
+  const { query } = useRouter();
+
   const { data, isLoading, error, refetch } = useQuery(
     ["myProfile"],
     async () => await getMyProfile(),
     {
       staleTime: 1000 * 60 * 5,
       cacheTime: 1000 * 60 * 30,
+      enabled: !query.id,
     }
   );
   return { data, isLoading, error, refetch };
