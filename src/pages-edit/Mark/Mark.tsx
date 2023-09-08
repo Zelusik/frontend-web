bf22abf707221b53e11bc651f7bb9d72bec7cd2d
cyeju <yeju1019@gmail.com>
2023-09-09 05:27:48 +0900

    fix: MarkTopNavigation (효율적이진 않음)

--- src/pages-edit/Mark/Mark.tsx
+++ src/pages-edit/Mark/Mark.tsx
@@ -9,20 +9,21 @@ import SearchTitle from "components/Title/SearchTitle";
 import useGetMarkKeywords from "hooks/queries/mark/useGetMarkKeywords";
 import Spacing from "components/Spacing";
 import useGetMarkPlaces from "hooks/queries/mark/useGetMarkPlaces";
-import Text from "components/Text";
-import NewButton from "pages-edit/Mypage/components/NewButton";
-import StoreCard from "./components/StoreCard";
+
 import BottomNavigation from "components/BottomNavigation";
 import { globalValue } from "constants/globalValue";
 import { Route } from "constants/Route";
 import useIntersectionObserver from "hooks/useIntersectionObserver";
 import LoadingCircle from "components/Loading/LoadingCircle";
+import StoreContainer from "./components/StoreContainer";
+import MarkTopNavigation from "components/TopNavigation/MarkTopNavigation";
 
 export default function Mark() {
   const router = useRouter();
   const scrollRef = useRef<any>(null);
   const infinityScrollRef = useRef<any>(null);
   const { height } = useDisplaySize();
+
   const [currentIndex, setCurrentIndex] = useState<number>(0);
   const [topFixed, setTopFixed] = useState<boolean>(false);
 
@@ -34,20 +35,15 @@ export default function Mark() {
     },
     ...keywordData?.keywords,
   ];
-  const keywordList = keywords?.map(
-    (keywordInfo: { keyword: string; type: string }) => keywordInfo.keyword
+  const titleList = keywords?.map(
+    (keywordInfo: { keyword: string; type: string }) => keywordInfo?.keyword
   );
 
-  const {
-    data: placeData,
-    fetchNextPage,
-    hasNextPage,
-  } = useGetMarkPlaces({
-    currentIndex,
+  const { markData, fetchNextPage, hasNextPage } = useGetMarkPlaces({
+    index: { idx: currentIndex, currentIndex },
     type: keywords?.[currentIndex]?.type,
     keyword: keywords?.[currentIndex]?.keyword,
   });
-
   useIntersectionObserver(infinityScrollRef, fetchNextPage, !!hasNextPage, {});
 
   function onScroll() {
@@ -65,10 +61,9 @@ export default function Mark() {
     return () => {
       scrollRef.current?.removeEventListener("scroll", onScroll);
     };
-  }, [currentIndex]);
+  }, [currentIndex, keywordData]);
 
   if (isLoading) return <LoadingCircle />;
-
   return (
     <>
       <SearchTitle type="mark" />
@@ -78,55 +73,52 @@ export default function Mark() {
         height={height - 50 - globalValue.BOTTOM_NAVIGATION_HEIGHT}
       >
         <Spacing size={20} />
-        <TopNavigation
+        <MarkTopNavigation
+          type="title-scroll"
+          scrollRef={scrollRef}
+          index={{ currentIndex, setCurrentIndex }}
+          top={{ topFixed, setTopFixed }}
+          scrollTop={20}
+          titleList={titleList}
+          count={markData?.[0]?.totalElements}
+        >
+          {titleList?.map((_: any, idx: number) => {
+            // if (markIsLoading) return <LoadingCircle key={idx} />;
+            return (
+              <TopNavigationInner key={idx} height={"auto"}>
+                <StoreContainer
+                  infinityScrollRef={infinityScrollRef}
+                  index={{ idx, currentIndex }}
+                  keywords={keywords}
+                />
+              </TopNavigationInner>
+            );
+          })}
+        </MarkTopNavigation>
+        {/* <TopNavigation
           type="mark"
           scrollRef={scrollRef}
           scrollTop={20}
           topFixed={topFixed}
           currentIndex={currentIndex}
           setCurrentIndex={setCurrentIndex}
-          titleList={keywordList}
-          count={placeData?.[0].totalElements ? placeData?.[0].totalElements : 0}
+          titleList={titleList}
+          // count={
+          //   placeData?.[0].totalElements ? placeData?.[0].totalElements : 0
+          // }
         >
-          {keywords?.map((_: any, idx: number) => {
+          {titleList?.map((_: any, idx: number) => {
             return (
               <TopNavigationInner key={idx} height={"auto"}>
-                {placeData?.[0].totalElements !== 0 ? (
-                  currentIndex === idx ? (
-                    <>
-                      <Spacing size={19} />
-                      <StoreWrapper>
-                        {placeData
-                          ?.flatMap((place_data) => place_data.contents)
-                          .map((place: any, placeIdx: number) => (
-                            <StoreCard key={placeIdx} placeInfo={place} />
-                          ))}
-
-                        <div ref={infinityScrollRef} />
-                        {hasNextPage ? (
-                          <>
-                            <LoadingCircle size={30} />
-                            <Spacing size={20} />
-                          </>
-                        ) : null}
-                      </StoreWrapper>
-                    </>
-                  ) : undefined
-                ) : (
-                  <NoContent>
-                    <Text typo="Paragraph5" color="N80">
-                      아직 저장된 음식점이 없습니다
-                    </Text>
-                    <NewButton
-                      buttonText="음식점 리뷰 둘러보기"
-                      onClick={() => router.push(Route.HOME())}
-                    />
-                  </NoContent>
-                )}
+                <StoreContainer
+                  infinityScrollRef={infinityScrollRef}
+                  index={{ idx, currentIndex }}
+                  keywords={keywords}
+                />
               </TopNavigationInner>
             );
           })}
-        </TopNavigation>
+        </TopNavigation> */}
       </Wrapper>
       <BottomNavigation />
     </>
@@ -138,24 +130,7 @@ const Wrapper = styled.div<{ height: number }>`
   background-color: #fbfbfb;
 `;
 
-const NoContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  gap: 20px;
-
-  height: calc(100vh - 227px);
-  text-align: center;
-`;
-
 const TopNavigationInner = styled.div<{ height: any }>`
   height: ${({ height }) => height};
-  padding: 0 20px;
   background-color: #fbfbfb;
-`;
-
-const StoreWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
 `;
