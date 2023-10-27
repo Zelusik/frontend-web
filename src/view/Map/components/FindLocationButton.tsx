bb7b3904a806824cc9327d11c2170975151be4f7
cyeju <yeju1019@gmail.com>
2023-10-27 23:52:45 +0900

    fix: map FindLocation 위치 수정

--- src/view/Map/components/FindLocationButton.tsx
+++ src/view/Map/components/FindLocationButton.tsx
@@ -1,4 +1,3 @@
-import { colors } from "constants/colors";
 import { globalValue } from "constants/globalValue";
 import Icon from "components/Icon";
 import { Box, Flex } from "components/core";
@@ -11,24 +10,19 @@ export default function FindLocationButton({
   handleClick,
 }: FindLocationButtonProps) {
   return (
-    <Flex
+    <Box
+      w={40}
+      h={40}
+      pt={11}
       pos="fixed"
-      bottom={`calc(${globalValue.BOTTOM_NAVIGATION_HEIGHT}px + 20px + 30%)`}
-      style={{ width: "100%", maxWidth: globalValue.MAX_WIDTH }}
+      right={20}
+      bg="N0"
+      radius="50%"
+      shadow="0px 0px 4px rgba(0, 0, 0, 0.12)"
+      bottom={`calc(${globalValue.BOTTOM_NAVIGATION_HEIGHT}px + 20px + 24%)`}
       onClick={handleClick}
     >
-      <Box
-        w={40}
-        h={40}
-        pt={11}
-        pos="absolute"
-        right={20}
-        bg="N0"
-        radius="50%"
-        shadow="0px 0px 4px rgba(0, 0, 0, 0.12)"
-      >
-        <Icon icon="FindLocation" />
-      </Box>
-    </Flex>
+      <Icon icon="FindLocation" />
+    </Box>
   );
 }
