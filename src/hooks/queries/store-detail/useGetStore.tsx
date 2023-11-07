3e7b3c4ac06476cb6060fc48e72db6e69a15a303
cyeju <yeju1019@gmail.com>
2023-11-07 20:29:26 +0900

    fix: search-place -> store-detail 이동시 에러 확인

--- src/hooks/queries/store-detail/useGetStore.tsx
+++ src/hooks/queries/store-detail/useGetStore.tsx
@@ -4,7 +4,7 @@ import { useAppSelector } from "hooks/useReduxHooks";
 import { getReviews } from "api/reviews";
 import { useState } from "react";
 
-const useGetStore = ({ kakaoId, placeId }: any): any => {
+const useGetStore = async ({ kakaoId, placeId }: any) => {
   const { placeInfo } = useAppSelector((state) => state.search);
   const [id, setId] = useState(null);
 
@@ -12,7 +12,7 @@ const useGetStore = ({ kakaoId, placeId }: any): any => {
   const getStoreInfo = async () => {
     if (kakaoId) {
       const isExistPlace = await placesApi.getExistence(kakaoId);
-      if (isExistPlace.existenceOfPlace) {
+      if (isExistPlace && isExistPlace.existenceOfPlace) {
         const res = await placesApi.getPlaces(kakaoId);
         setId(res.id);
         return res;
@@ -26,53 +26,136 @@ const useGetStore = ({ kakaoId, placeId }: any): any => {
     }
   };
 
-  const {
-    data: storeInfoData,
-    isLoading: isStoreInfoLoading,
-    error,
-    refetch,
-  } = useQuery(["search"], getStoreInfo, {
-    staleTime: 1000 * 60 * 5,
-    cacheTime: 1000 * 60 * 30,
-  });
+  const newId = await getStoreInfo();
 
-  // 리뷰 목록 조회
-  const getReviewsWithParams = ({ pageParam = 0 }) => {
-    console.log("A");
-    return getReviews({
-      params: {
-        placeId: placeId || id,
-        page: pageParam,
-        size: 10,
-        embed: "WRITER",
-      },
-    });
-  };
+  return {};
 
-  const {
-    data,
-    fetchNextPage,
-    hasNextPage,
-    isLoading: isReviewsLoading,
-  } = useInfiniteQuery(["reviews", placeId], getReviewsWithParams, {
-    enabled: Boolean(!isStoreInfoLoading && id),
-    getNextPageParam: (lastPage) => {
-      return lastPage.isLast ? undefined : lastPage.number + 1;
-    },
-  });
-  const reviewDatas = data?.pages;
-  console.log("B");
+  // const {
+  //   data: storeInfoData,
+  //   isLoading: isStoreInfoLoading,
+  //   error,
+  //   refetch,
+  // } = useQuery(["search"], getStoreInfo, {
+  //   staleTime: 1000 * 60 * 5,
+  //   cacheTime: 1000 * 60 * 30,
+  // });
 
-  return {
-    storeInfoData,
-    isStoreInfoLoading,
-    error,
-    refetch,
-    reviewDatas,
-    fetchNextPage,
-    hasNextPage,
-    isReviewsLoading,
-  };
+  // // 리뷰 목록 조회
+  // const getReviewsWithParams = ({ pageParam = 0 }) => {
+  //   console.log("A");
+  //   return getReviews({
+  //     params: {
+  //       placeId: placeId || id,
+  //       page: pageParam,
+  //       size: 10,
+  //       embed: "WRITER",
+  //     },
+  //   });
+  // };
+
+  // const {
+  //   data,
+  //   fetchNextPage,
+  //   hasNextPage,
+  //   isLoading: isReviewsLoading,
+  // } = useInfiniteQuery(["reviews", placeId], getReviewsWithParams, {
+  //   enabled: Boolean(!isStoreInfoLoading && id),
+  //   getNextPageParam: (lastPage) => {
+  //     return lastPage.isLast ? undefined : lastPage.number + 1;
+  //   },
+  // });
+  // const reviewDatas = data?.pages;
+  // console.log("B");
+
+  // return {
+  //   storeInfoData,
+  //   isStoreInfoLoading,
+  //   error,
+  //   refetch,
+  //   reviewDatas,
+  //   fetchNextPage,
+  //   hasNextPage,
+  //   isReviewsLoading,
+  // };
 };
 
 export default useGetStore;
+
+// import { useInfiniteQuery, useQuery } from "react-query";
+// import { placesApi } from "api/places";
+// import { useAppSelector } from "hooks/useReduxHooks";
+// import { getReviews } from "api/reviews";
+// import { useState } from "react";
+
+// const useGetStore = ({ kakaoId, placeId }: any): any => {
+//   const { placeInfo } = useAppSelector((state) => state.search);
+//   const [id, setId] = useState(null);
+
+//   // 음식점 정보
+//   const getStoreInfo = async () => {
+//     if (kakaoId) {
+//       const isExistPlace = await placesApi.getExistence(kakaoId);
+//       if (isExistPlace.existenceOfPlace) {
+//         const res = await placesApi.getPlaces(kakaoId);
+//         setId(res.id);
+//         return res;
+//       } else {
+//         const res = await placesApi.postPlaces(placeInfo);
+//         setId(res.id);
+//         return res;
+//       }
+//     } else {
+//       return await placesApi.getPlacesId(placeId);
+//     }
+//   };
+
+//   const {
+//     data: storeInfoData,
+//     isLoading: isStoreInfoLoading,
+//     error,
+//     refetch,
+//   } = useQuery(["search"], getStoreInfo, {
+//     staleTime: 1000 * 60 * 5,
+//     cacheTime: 1000 * 60 * 30,
+//   });
+
+//   // 리뷰 목록 조회
+//   const getReviewsWithParams = ({ pageParam = 0 }) => {
+//     console.log("A");
+//     return getReviews({
+//       params: {
+//         placeId: placeId || id,
+//         page: pageParam,
+//         size: 10,
+//         embed: "WRITER",
+//       },
+//     });
+//   };
+
+//   const {
+//     data,
+//     fetchNextPage,
+//     hasNextPage,
+//     isLoading: isReviewsLoading,
+//   } = useInfiniteQuery(["reviews", placeId], getReviewsWithParams, {
+//     enabled: Boolean(!isStoreInfoLoading && id),
+//     getNextPageParam: (lastPage) => {
+//       return lastPage.isLast ? undefined : lastPage.number + 1;
+//     },
+//   });
+//   const reviewDatas = data?.pages;
+//   console.log("B");
+
+//   return {
+//     storeInfoData,
+//     isStoreInfoLoading,
+//     error,
+//     refetch,
+//     reviewDatas,
+//     fetchNextPage,
+//     hasNextPage,
+//     isReviewsLoading,
+//   };
+// };
+
+// export default useGetStore;
