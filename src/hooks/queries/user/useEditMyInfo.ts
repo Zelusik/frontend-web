f01ebde6e3893d4cf5b19a673eac71aba7d2507b
gabi <70315572+Limgabi@users.noreply.github.com>
2024-02-06 17:12:39 +0900

    fix #126 : 프로필 수정 버튼 disabled 조건 추가

--- src/hooks/queries/user/useEditMyInfo.ts
+++ src/hooks/queries/user/useEditMyInfo.ts
@@ -1,17 +1,17 @@
-import { editMyInfo } from "api/members";
-import { useMutation, useQueryClient } from "react-query";
+import { editMyInfo } from 'api/members';
+import { useMutation, useQueryClient } from 'react-query';
 
 const useEditMyInfo = () => {
   const queryClient = useQueryClient();
 
-  const { mutate } = useMutation(editMyInfo, {
-    mutationKey: ["user"],
+  const { mutate, isLoading } = useMutation(editMyInfo, {
+    mutationKey: ['user'],
     onSuccess: () => {
-      queryClient.invalidateQueries(["user"]);
+      queryClient.invalidateQueries(['user']);
     },
   });
 
-  return { mutate };
+  return { mutate, isLoading };
 };
 
 export default useEditMyInfo;
